694b12ecb8a8808a5586ec598824a77ad88a3bbc
Igor Stepanov <stipyk1309@gmail.com>
2024-03-29 18:33:26 +0300

    feat: sort items

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Item from './Item'
 import { IToDo } from '../types'
 
@@ -8,13 +8,59 @@ interface IList {
 }
 
 const List: React.FC<IList> = ({ items, setItems }) => {
+	const [sortBy, setSortBy] = useState('input')
+
+	let sortedItems
+
+	if (sortBy === 'input') sortedItems = items
+
+	if (sortBy === 'description')
+		sortedItems = items
+			.slice()
+			.sort((a, b) => a.description.localeCompare(b.description))
+
+	if (sortBy === 'completed')
+		sortedItems = items
+			.slice()
+			.sort((a, b) => Number(a.completed) - Number(b.completed))
+
+	const handleSort = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		setSortBy(e.target.value)
+	}
+
+	const handleClear = () => {
+		const confirmed = window.confirm(
+			'Are you  sure you want to delete all dotos?'
+		)
+
+		if (confirmed) {
+			setItems([])
+			setSortBy('input')
+		}
+	}
+
 	return (
 		<div className='list'>
 			<ul>
-				{items.map(item => (
+				{sortedItems?.map(item => (
 					<Item item={item} setItems={setItems} key={item.id} />
 				))}
 			</ul>
+			<div className='actions'>
+				<select
+					name='actions'
+					id='actions'
+					value={sortBy}
+					onChange={e => handleSort(e)}
+				>
+					<option value='input'>Sort by input order</option>
+					<option value='description'>Sort by description order</option>
+					<option value='completed'>Sort by completed status</option>
+				</select>
+				<button type='button' onClick={handleClear}>
+					Clear
+				</button>
+			</div>
 		</div>
 	)
 }
